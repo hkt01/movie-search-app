dd1ddcc75bc1305f19bd8d7c380d37991799a631
Katariina Tiitinen (TAU) <katariina.tiitinen@tuni.fi>
2019-12-16 15:37:55 +0200

    Build added

---
+++ frontend/build/precache-manifest.ed71437ca5c1bc4d1816fb65a8bd89e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d69876c28f1b3d4a2e25cd7579f50760",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc8269bebc819a5e3f61",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "60c56648b38cac6f106c",
+    "url": "/static/css/main.34d066ce.chunk.css"
+  },
+  {
+    "revision": "dc8269bebc819a5e3f61",
+    "url": "/static/js/2.b292fb1c.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.b292fb1c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "60c56648b38cac6f106c",
+    "url": "/static/js/main.af9a6a6a.chunk.js"
+  },
+  {
+    "revision": "810b7eddbba2845a5756",
+    "url": "/static/js/runtime-main.4e64986e.js"
+  }
+]);
